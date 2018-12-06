29d3b8a90f1bca4b937a50333d555811b3b9e233
maveric-ui <maveric.ui@gmail.com>
2018-12-06 17:57:31 +0200

    start

---
+++ src/app/modules/authentication/authentication.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthenticationComponent } from './components/authentication/authentication.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [AuthenticationComponent]
+})
+export class AuthenticationModule { }
