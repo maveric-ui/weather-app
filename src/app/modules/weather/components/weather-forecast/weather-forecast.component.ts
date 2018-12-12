7c976bd7b26558c90e74b388f2c3e6def7966b3d
maveric-ui <lokhmanov.maksym@gmail.com>
2018-12-12 22:13:18 +0200

    TODO: charts

--- src/app/modules/weather/components/weather-forecast/weather-forecast.component.ts
+++ src/app/modules/weather/components/weather-forecast/weather-forecast.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit, ViewEncapsulation, OnChanges, SimpleChanges } from '@angular/core';
 import Chart from 'chart.js';
 import { ForecastArray, ForecastObject } from '../../classes/weather-forecast';
+import { MatTabChangeEvent } from '@angular/material';
 
 @Component({
   selector: 'app-weather-forecast',
@@ -11,7 +12,8 @@ import { ForecastArray, ForecastObject } from '../../classes/weather-forecast';
 export class WeatherForecastComponent implements OnInit, OnChanges {
 
   @Input() private forecastData: ForecastArray[];
-  public chartForecast: Chart;
+  public chartWind;
+  public chartTemp;
   private windForecast: number[];
   private tempForecast: number[];
   private pressureForecast: number[];
@@ -23,11 +25,14 @@ export class WeatherForecastComponent implements OnInit, OnChanges {
   ngOnChanges(changes: SimpleChanges) {
     if (changes.forecastData && !changes.forecastData.isFirstChange()) {
       this.getForecastData(changes.forecastData.currentValue);
-      this.chartInit();
+      this.chartWindInit();
+      this.chartTempInit();
     }
   }
 
   ngOnInit() {}
+
+  handleTabChanges(event: MatTabChangeEvent) {}
 
   getForecastData(forecastData) {
     this.windForecast = forecastData.map((data: ForecastObject) => data.wind.speed);
@@ -40,15 +45,14 @@ export class WeatherForecastComponent implements OnInit, OnChanges {
     });
   }
 
-  chartInit() {
-    const data = {
+  chartWindInit() {
+    const dataWind = {
       labels: this.allDates,
       datasets: [{
-        label: 'Forecast',
-        data: this.tempForecast,
+        label: 'Wind',
+        data: this.windForecast,
         borderColor: '#ff9191',
         backgroundColor: '#ddd',
-        height: '200px',
         fill: 'start'
       }]
     };
@@ -67,11 +71,44 @@ export class WeatherForecastComponent implements OnInit, OnChanges {
       }
     };
 
-    this.chartForecast = new Chart('canvasTemp', {
+    this.chartWind = new Chart('canvasWind', {
       type: 'line',
-      data: {...data},
+      data: {...dataWind},
       options: {...options}
     });
   }
 
+  chartTempInit() {
+    const dataTemp = {
+      labels: this.allDates,
+      datasets: [{
+        label: 'Temp',
+        data: this.tempForecast,
+        borderColor: '#ff9191',
+        backgroundColor: '#ddd',
+        fill: 'start'
+      }]
+    };
+
+    const options = {
+      legend: {
+        display: false,
+      },
+      scalse: {
+        xAxes: [{
+          display: true
+        }],
+        yAxes: [{
+          display: true
+        }]
+      }
+    };
+
+    this.chartTemp = new Chart('canvasTemp', {
+      type: 'line',
+      data: {...dataTemp},
+      options: {...options}
+    });
+
+  }
 }
